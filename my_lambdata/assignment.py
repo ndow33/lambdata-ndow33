b0da02000898c31cf52cd057c3e0bfa9e2c8108d
Nate Dow <nate.dow@go1.com>
2020-05-07 20:50:10 -0600

    State codes step one

---
+++ my_lambdata/assignment.py
@@ -0,0 +1,25 @@
+
+
+#Convert State abbreviations to state names
+from pandas import DataFrame
+
+def add_state_names(my_df):
+    #add a column of corresponding state names
+    #dict with abbrev/name mappings
+    
+    new_df = my_df.copy()
+    
+    names_map = {"CA":"Cali", "CO":"Colo", "CT":"Conn"}
+
+    new_df["name"] = new_df["abbrev"].map(names_map)
+
+
+
+    return new_df
+
+if __name__ == "__main__":
+    df = DataFrame({"abbrev":["CA","CO","CT","DC","TX"]})
+    print(df.head())
+
+    df2 = add_state_names(df)
+    print(df2)
--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,11 +3,19 @@
 def enlarge(n):
     return n * 100
 
+#Check a dataframe for nulls, print/report them in a nice "pretty" format
 def check_nulls(df):
     total = df.isna().sum()
     total = total.sum()
     string = f"There are {total} null values in your dataframe"
     return string
+
+#Single function to take a list, turn it into a series
+#and add it to a dataframe as a new column
+def add_column(a_list, dataframe, feature):
+  a_list = pd.Series(a_list)
+  dataframe[feature] = a_list
+  return dataframe
 
 
 if __name__ == "__main__":
@@ -16,6 +24,8 @@ if __name__ == "__main__":
 
     
     x = 6 
+
+
     print(enlarge(x))
 
     y = int(input("Please choose an integer like 5: "))
