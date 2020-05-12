e46b5cd791d8fa65f01058dc319787b85b204e85
Nate Dow <nate.dow@go1.com>
2020-05-11 20:42:52 -0600

    Inheritance

---
+++ my_lambdata/assignment_inheritance.py
@@ -0,0 +1,18 @@
+# Convert State abbreviations to state names
+from pandas import DataFrame
+
+class MyFrame(DataFrame):
+
+    def add_state_names(self):
+    # add a column of corresponding state names
+    # dict with abbrev/name mappings
+        names_map = {"CA": "Cali", "CO": "Colo", "CT": "Conn"}
+        self["name"] = self["abbrev"].map(names_map)
+
+if __name__ == "__main__":
+    myframe = MyFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
+    print(myframe.head())
+    print(myframe.columns)
+
+    myframe.add_state_names()
+    print(myframe.head())
--- my_lambdata/assignment_oop.py
+++ my_lambdata/assignment_oop.py
@@ -1,18 +1,23 @@
 # Convert State abbreviations to state names
 from pandas import DataFrame
 
+class DataProcessor():
+    def __init__(self, my_df): # constructor
+        self.df = my_df
 
-def add_state_names(my_df):
+    def add_state_names(self):
     # add a column of corresponding state names
     # dict with abbrev/name mappings
-    new_df = my_df.copy()
-    names_map = {"CA": "Cali", "CO": "Colo", "CT": "Conn"}
-    new_df["name"] = new_df["abbrev"].map(names_map)
-    return new_df
+        names_map = {"CA": "Cali", "CO": "Colo", "CT": "Conn"}
+        self.df["name"] = self.df["abbrev"].map(names_map)
+        return self.df
 
 if __name__ == "__main__":
     df = DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
     print(df.head())
 
-    df2 = add_state_names(df)
-    print(df2)
+    processor = DataProcessor(df)
+    print(processor.df.head())
+
+    processor.add_state_names()
+    print(processor.df.head())
